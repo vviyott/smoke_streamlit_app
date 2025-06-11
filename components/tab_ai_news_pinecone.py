5b046013d802bd12678011aa31646f28ecaeb68b
seyoung hwang <vviyott@naver.com>
2025-06-11 17:47:41 +0900

    Update tab_ai_news_pinecone.py

--- components/tab_ai_news_pinecone.py
+++ components/tab_ai_news_pinecone.py
@@ -1,4 +1,8 @@
 # components/tab_ai_news_pinecone.py
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from pinecone import Pinecone
 from openai import OpenAI
