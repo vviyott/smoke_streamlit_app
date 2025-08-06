44535f36fab16500647ffb9556da69e69f09b626
seyoung hwang <vviyott@naver.com>
2025-08-06 09:58:44 +0900

    Update tab_dash.py

--- components/tab_dash.py
+++ components/tab_dash.py
@@ -10,15 +10,20 @@ def seoul_smoking_rate_2022():
     자치구별 흡연율 순위와 흡연 현황 지도, 성별 흡연율 통계를 함께 확인해보세요.
     """)
     
-    # Tableau Public URL with all parameters for full display
-    full_url = "https://public.tableau.com/views/SmokingrateinSeoul2022/1?:language=ko-KR&:display_count=n&:origin=viz_share_link&:embed=y&:showVizHome=no&:toolbar=top&:animate_transition=yes&:display_static_image=no&:display_spinner=yes&:display_overlay=yes&:display_count=yes&:tabs=no"
+    # 완전한 대시보드를 보여주기 위해 매우 큰 높이 설정
+    embed_url = "https://public.tableau.com/views/SmokingrateinSeoul2022/1?:language=ko-KR&:display_count=n&:origin=viz_share_link&:embed=y&:showVizHome=no&:toolbar=top&:animate_transition=yes&:display_static_image=no&:display_spinner=yes&:display_overlay=yes&:display_count=yes"
     
-    # Streamlit 내장 iframe 사용
-    st.components.v1.iframe(
-        src=full_url,
-        width=None,  # 자동 너비 조정
-        height=1600,  # 충분한 높이
-        scrolling=False
+    components.html(
+        f"""
+        <iframe src="{embed_url}" 
+                width="100%" 
+                height="1600" 
+                frameborder="0"
+                allowtransparency="true"
+                allowfullscreen="true"
+                style="border: none; display: block; margin: 0 auto;">
+        </iframe>
+        """,
+        height=1650
     )
-    
     st.caption("출처: [서울 열린데이터 광장](https://data.seoul.go.kr/dataList/10668/S/2/datasetView.do#)")
