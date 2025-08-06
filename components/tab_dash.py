2c36297db0db019b2ee8154ab872eae07e3bbc75
seyoung hwang <vviyott@naver.com>
2025-08-06 09:38:15 +0900

    Update tab_dash.py

--- components/tab_dash.py
+++ components/tab_dash.py
@@ -8,66 +8,46 @@ def seoul_smoking_rate_2022():
     자치구별 흡연율 순위와 흡연 현황 지도, 성별 흡연율 통계를 함께 확인해보세요.
     """)
     
-    # 방법 1: CSS로 중앙 정렬 적용
-    components.html(
-        """
-        <style>
-        .tableauPlaceholder {
-            display: flex !important;
-            justify-content: center !important;
-            align-items: center !important;
-            width: 100% !important;
-            margin: 0 auto !important;
-        }
-        .tableauViz {
-            margin: 0 auto !important;
-            display: block !important;
-        }
-        </style>
-        <div class='tableauPlaceholder' id='viz1754439309658' 
-             style='position: relative; width: 100%; max-width: 1200px; margin: 0 auto;'>
-          <noscript>
-            <a href='#'>
-              <img alt='대시보드 1 '
-                   src='https://public.tableau.com/static/images/Sm/SmokingrateinSeoul2022/1/1_rss.png'
-                   style='border: none; margin: 0 auto; display: block;' />
-            </a>
-          </noscript>
-          <object class='tableauViz' style='display:none; width: 100%; height: 877px; margin: 0 auto;'>
-            <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-            <param name='embed_code_version' value='3' />
-            <param name='site_root' value='' />
-            <param name='name' value='SmokingrateinSeoul2022/1' />
-            <param name='tabs' value='no' />
-            <param name='toolbar' value='yes' />
-            <param name='static_image' value='https://public.tableau.com/static/images/Sm/SmokingrateinSeoul2022/1/1.png' />
-            <param name='animate_transition' value='yes' />
-            <param name='display_static_image' value='yes' />
-            <param name='display_spinner' value='yes' />
-            <param name='display_overlay' value='yes' />
-            <param name='display_count' value='yes' />
-            <param name='language' value='ko-KR' />
-          </object>
-        </div>
-        <script type='text/javascript'>
-          var divElement = document.getElementById('viz1754439309658');
-          var vizElement = divElement.getElementsByTagName('object')[0];
-          if (divElement.offsetWidth > 800) {
-            vizElement.style.width='100%';
-            vizElement.style.height='877px';
-          } else if (divElement.offsetWidth > 500) {
-            vizElement.style.width='100%';
-            vizElement.style.height='827px';
-          } else {
-            vizElement.style.width='100%';
-            vizElement.style.height='727px';
-          }
-          var scriptElement = document.createElement('script');
-          scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-          vizElement.parentNode.insertBefore(scriptElement, vizElement);
-        </script>
-        """,
-        height=950
-    )
+    # 좌우 여백을 두고 중앙에 배치
+    col1, col2, col3 = st.columns([0.5, 10, 0.5])
+    
+    with col2:
+        components.html(
+            """
+            <div class='tableauPlaceholder' id='viz1754439309658' 
+                 style='position: relative; width: 100%; margin: 0 auto;'>
+              <noscript>
+                <a href='#'>
+                  <img alt='대시보드 1 '
+                       src='https://public.tableau.com/static/images/Sm/SmokingrateinSeoul2022/1/1_rss.png'
+                       style='border: none' />
+                </a>
+              </noscript>
+              <object class='tableauViz' style='display:none; width: 100%; height: 877px;'>
+                <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+                <param name='embed_code_version' value='3' />
+                <param name='site_root' value='' />
+                <param name='name' value='SmokingrateinSeoul2022/1' />
+                <param name='tabs' value='no' />
+                <param name='toolbar' value='yes' />
+                <param name='static_image' value='https://public.tableau.com/static/images/Sm/SmokingrateinSeoul2022/1/1.png' />
+                <param name='animate_transition' value='yes' />
+                <param name='display_static_image' value='yes' />
+                <param name='display_spinner' value='yes' />
+                <param name='display_overlay' value='yes' />
+                <param name='display_count' value='yes' />
+                <param name='language' value='ko-KR' />
+              </object>
+            </div>
+            <script type='text/javascript'>
+              var divElement = document.getElementById('viz1754439309658');
+              var vizElement = divElement.getElementsByTagName('object')[0];
+              var scriptElement = document.createElement('script');
+              scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+              vizElement.parentNode.insertBefore(scriptElement, vizElement);
+            </script>
+            """,
+            height=950
+        )
     
     st.caption("출처: [서울 열린데이터 광장](https://data.seoul.go.kr/dataList/10668/S/2/datasetView.do#)")
