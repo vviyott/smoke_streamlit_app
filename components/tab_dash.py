21860d69d22cb046838794291d7dc340f2dac719
seyoung hwang <vviyott@naver.com>
2025-08-06 09:46:38 +0900

    Update tab_dash.py

--- components/tab_dash.py
+++ components/tab_dash.py
@@ -1,51 +1,29 @@
+
 import streamlit as st
 import streamlit.components.v1 as components
 
-def seoul_smoking_rate_2022():
+# 방법 1: iframe 사용 (가장 안정적)
+def seoul_smoking_rate_2022_iframe():
     st.markdown("## 서울시민 흡연율 시각화📈")
     st.markdown("""
     2022년 서울시 자치구별 흡연율 데이터를 시각화한 자료입니다.  
     자치구별 흡연율 순위와 흡연 현황 지도, 성별 흡연율 통계를 함께 확인해보세요.
     """)
-
+    
+    # Tableau Public의 직접 embed URL 사용
+    embed_url = "https://public.tableau.com/views/SmokingrateinSeoul2022/1?:language=ko-KR&:display_count=n&:origin=viz_share_link&:embed=y&:showVizHome=no&:toolbar=top"
+    
     components.html(
-        """
-        <div class='tableauPlaceholder' id='viz1754439309658' style='position: relative'>
-          <noscript>
-            <a href='#'>
-              <img alt='대시보드 1 '
-                   src='https://public.tableau.com/static/images/Sm/SmokingrateinSeoul2022/1/1_rss.png'
-                   style='border: none' />
-            </a>
-          </noscript>
-          <object class='tableauViz' style='display:none;'>
-            <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-            <param name='embed_code_version' value='3' />
-            <param name='site_root' value='' />
-            <param name='name' value='SmokingrateinSeoul2022/1' />
-            <param name='tabs' value='no' />
-            <param name='toolbar' value='yes' />
-            <param name='static_image' value='https://public.tableau.com/static/images/Sm/SmokingrateinSeoul2022/1/1.png' />
-            <param name='animate_transition' value='yes' />
-            <param name='display_static_image' value='yes' />
-            <param name='display_spinner' value='yes' />
-            <param name='display_overlay' value='yes' />
-            <param name='display_count' value='yes' />
-            <param name='language' value='ko-KR' />
-          </object>
-        </div>
-        <script type='text/javascript'>
-          var divElement = document.getElementById('viz1754439309658');
-          var vizElement = divElement.getElementsByTagName('object')[0];
-          vizElement.style.width = '2400px';
-          vizElement.style.height = '877px';
-          var scriptElement = document.createElement('script');
-          scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-          vizElement.parentNode.insertBefore(scriptElement, vizElement);
-        </script>
+        f"""
+        <iframe src="{embed_url}" 
+                width="100%" 
+                height="1200" 
+                frameborder="0"
+                allowtransparency="true"
+                allowfullscreen="true"
+                style="border: none;">
+        </iframe>
         """,
-        height=950
+        height=1220
     )
-
     st.caption("출처: [서울 열린데이터 광장](https://data.seoul.go.kr/dataList/10668/S/2/datasetView.do#)")
-
