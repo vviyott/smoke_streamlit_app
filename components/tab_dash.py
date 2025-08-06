41b1f91124d059c947e1d54c8821938f57bb5851
seyoung hwang <vviyott@naver.com>
2025-08-06 09:12:22 +0900

    Update tab_dash.py

--- components/tab_dash.py
+++ components/tab_dash.py
@@ -1,4 +1,3 @@
-# components/tab_dash.py (v0)
 import streamlit as st
 import streamlit.components.v1 as components
 
@@ -9,41 +8,20 @@ def seoul_smoking_rate_2022():
     2022년 서울시 자치구별 흡연율 데이터를 시각화한 자료입니다.  
     자치구별 흡연율 순위와 흡연 현황 지도, 성별 흡연율 통계를 함께 확인해보세요.
     """)
-    # st.markdown("---")
 
+    # ✅ Tableau Public iframe 임베딩
     components.html(
         """
-        <div id='vizSmoking' class='tableauPlaceholder' style='width:100%;'>
-          <noscript>
-            <a href='#'>
-              <img alt='흡연율 시각화'
-                  src='https://public.tableau.com/static/images/Sm/SmokingrateinSeoul2022/1/1.png'
-                  style='border: none' />
-            </a>
-          </noscript>
-
-          <object class='tableauViz' style='display:none;'>
-            <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F'/>
-            <param name='embed_code_version' value='3'/>
-            <param name='site_root' value=''/>
-            <param name='name' value='SmokingrateinSeoul2022/1'/>
-            <param name='tabs' value='no'/>
-            <param name='toolbar' value='yes'/>
-            <param name='language' value='ko-KR'/>
-          </object>
-        </div>
-
-        <script src='https://public.tableau.com/javascripts/api/viz_v1.js'></script>
-        <script>
-          const div = document.getElementById('vizSmoking');
-          const viz = div.getElementsByTagName('object')[0];
-          viz.style.width = '1000px';  // 💡 width를 고정!
-          viz.style.height = '700px';  // 💡 height도 고정!
-        </script>
+        <iframe 
+            src="https://public.tableau.com/views/SmokingrateinSeoul2022/1?:language=ko-KR&:display_count=y&:origin=viz_share_link"
+            width="1000" 
+            height="700" 
+            style="border:none;" 
+            frameborder="0" 
+            allowfullscreen>
+        </iframe>
         """,
-        height=900,  # 첫 로딩 때 공간 확보용 (JS가 재조정하긴 함)
-        scrolling=False
+        height=720  # Streamlit 내부에서 공간 확보용
     )
 
     st.caption("출처: [서울 열린데이터 광장](https://data.seoul.go.kr/dataList/10668/S/2/datasetView.do#)")
-
