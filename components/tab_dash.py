a07d21b5e319f394629f94eca6ce124b6039c478
seyoung hwang <vviyott@naver.com>
2025-08-06 09:57:50 +0900

    Update tab_dash.py

--- components/tab_dash.py
+++ components/tab_dash.py
@@ -10,18 +10,15 @@ def seoul_smoking_rate_2022():
     자치구별 흡연율 순위와 흡연 현황 지도, 성별 흡연율 통계를 함께 확인해보세요.
     """)
     
-    # 정적 이미지 표시
-    st.image(
-        "https://public.tableau.com/static/images/Sm/SmokingrateinSeoul2022/1/1.png",
-        caption="서울시 2022년 흡연율 데이터 시각화",
-        use_column_width=True
+    # Tableau Public URL with all parameters for full display
+    full_url = "https://public.tableau.com/views/SmokingrateinSeoul2022/1?:language=ko-KR&:display_count=n&:origin=viz_share_link&:embed=y&:showVizHome=no&:toolbar=top&:animate_transition=yes&:display_static_image=no&:display_spinner=yes&:display_overlay=yes&:display_count=yes&:tabs=no"
+    
+    # Streamlit 내장 iframe 사용
+    st.components.v1.iframe(
+        src=full_url,
+        width=None,  # 자동 너비 조정
+        height=1600,  # 충분한 높이
+        scrolling=False
     )
-    
-    # 인터랙티브 버전 링크
-    st.markdown("""
-    **📊 [인터랙티브 버전 보기](https://public.tableau.com/views/SmokingrateinSeoul2022/1?:language=ko-KR&:display_count=n&:origin=viz_share_link)**
-    
-    *위 링크를 클릭하면 Tableau Public에서 전체 대시보드를 인터랙티브하게 이용할 수 있습니다.*
-    """)
     
     st.caption("출처: [서울 열린데이터 광장](https://data.seoul.go.kr/dataList/10668/S/2/datasetView.do#)")
